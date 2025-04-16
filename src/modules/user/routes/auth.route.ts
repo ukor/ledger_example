b544d970e11efc05042d4d89ec596e99b411020f
Jidechi ukor <10085056+ukor@users.noreply.github.com>
2025-04-16 10:03:19 +0100

    added login route

--- src/modules/user/routes/auth.route.ts
+++ src/modules/user/routes/auth.route.ts
@@ -4,7 +4,10 @@ import {
   HttpErrorResponseDto,
   HttpResponseDto,
 } from "../../../commons/dtos/http_response.dto";
-import { SignUpCredentials } from "../dtos/auth_credentials.dto";
+import {
+  LoginCredentials,
+  SignUpCredentials,
+} from "../dtos/auth_credentials.dto";
 import { AuthRepository } from "../repositories/auth.repository";
 import { AuthService } from "../services/auth.service";
 
@@ -39,6 +42,43 @@ export default async function(fastify: FastifyInstance) {
         const authService = new AuthService(authRepo);
 
         const r = await authService.signUp(request.body);
+
+        reply.status(200).send({
+          message: r.message,
+          constext: "ok",
+          payload: r.payload,
+        });
+      },
+    });
+  });
+
+  fastify.after(() => {
+    fastify.withTypeProvider<ZodTypeProvider>().route({
+      method: "POST",
+      url: "/n/v1/login",
+      schema: {
+        security: [{ jwt: [], uid: [], role: [], did: [] }],
+        body: LoginCredentials,
+        description:
+          "Allows existing user to authenticate to the ledger network",
+        tags: ["authentication"],
+        response: {
+          "2xx": HttpResponseDto,
+          "3xx": HttpErrorResponseDto,
+          "4xx": HttpErrorResponseDto,
+          "5xx": HttpErrorResponseDto,
+        },
+      },
+
+      handler: async (
+        request: FastifyRequest<{ Body: SignUpCredentials; }>,
+        reply: FastifyReply,
+      ) => {
+        /// ---
+
+        const authService = new AuthService(authRepo);
+
+        const r = await authService.login(request.body);
 
         reply.status(200).send({
           message: r.message,
